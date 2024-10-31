58bbf60256d487bf7d4128d157fdfa9ae6cdf433
Andhavarapu Balu <balu.somenumbers@gmail.com>
2024-10-31 20:54:05 +0530

    added comparison with regular spectral clustering using the same hyperparameters

--- example1.py
+++ example1.py
@@ -1,5 +1,5 @@
 """
-This script shows how to run the spectral equal size clustering.
+This script shows how to run the spectral equal size clustering and regular spectral clustering side by side.
 From a set of hyperparameters, you get clusters with size roughly equal to N/ncluster
 """
 import pandas as pd
@@ -7,6 +7,7 @@ import numpy as np
 import logging
 from source_code.spectral_equal_size_clustering import SpectralEqualSizeClustering
 from source_code.visualisation import visualise_clusters
+from sklearn.cluster import SpectralClustering
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
@@ -17,18 +18,38 @@ coords = pd.read_csv("datasets/restaurants_in_amsterdam.csv")
 # read the file of the symmetric distance matrix associated to the coords data frame
 dist_tr = np.load("datasets/symmetric_dist_tr.npy")
 
-clustering = SpectralEqualSizeClustering(nclusters=6,
-                                         nneighbors=int(dist_tr.shape[0] * 0.1),
-                                         equity_fraction=1,
-                                         seed=1234)
+# Spectral Equal Size Clustering
+equal_size_clustering = SpectralEqualSizeClustering(nclusters=6,
+                                                    nneighbors=int(dist_tr.shape[0] * 0.1),
+                                                    equity_fraction=1,
+                                                    seed=1234)
+equal_size_labels = equal_size_clustering.fit(dist_tr)
+coords["equal_size_cluster"] = equal_size_labels
+logging.info(f"Points per equal size cluster: \n {coords.equal_size_cluster.value_counts()}")
 
-labels = clustering.fit(dist_tr)
+# Regular Spectral Clustering
+spectral_clustering = SpectralClustering(n_clusters=6,
+                                                assign_labels="discretize",
+                                                n_neighbors=int(dist_tr.shape[0] * 0.1),
+                                                affinity="precomputed_nearest_neighbors",
+                                                random_state=1234)
+regular_labels = spectral_clustering.fit_predict(dist_tr)
+coords["regular_cluster"] = regular_labels
+logging.info(f"Points per regular cluster: \n {coords.regular_cluster.value_counts()}")
 
-coords["cluster"] = labels
-logging.info(f"Points per cluster: \n {coords.cluster.value_counts()}")
-clusters_figure = visualise_clusters(coords,
-                                     longitude_colname="longitude",
-                                     latitude_colname="latitude",
-                                     label_col="cluster",
-                                     zoom=11)
-clusters_figure.show()
+
+# Equal Size Clustering Visualization
+equal_size_clusters_figure = visualise_clusters(coords,
+                                                longitude_colname="longitude",
+                                                latitude_colname="latitude",
+                                                label_col="equal_size_cluster",
+                                                zoom=11)
+equal_size_clusters_figure.show()
+
+# Regular Spectral Clustering Visualization
+regular_clusters_figure = visualise_clusters(coords,
+                                             longitude_colname="longitude",
+                                             latitude_colname="latitude",
+                                             label_col="regular_cluster",
+                                             zoom=11)
+regular_clusters_figure.show()
